aadbdb18315fd6232d034e6f76cf05bfdbe85979
Jessica Perez <1jessicaperez@gmail.com>
2020-09-05 11:25:02 -0400

    README added

---
+++ README.md
@@ -0,0 +1,40 @@
+# Express: NOTE TAKER
+
+## Description
+
+This application can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note data.
+
+## Table of Contents
+
+1. [Installation](#Installation)
+2. [Usage](#Usage)
+3. [Github repository and Heroku deployed app link](#Github-repository-Heroku-deployed-link)
+4. [App-screenshots](#App-screenshots)
+5. [Questions](#Questions)
+
+## Installation
+
+- Make sure to `npm init` to get the `package.json` file, then `npm install` in your terminal.
+
+## Usage
+
+- Once installed, run `node server.js` in your teminal to get the app started. After opening the local host port in the browser, you can get started.
+- Or you can simply click on the Heroku deployed app link and start taking notes!
+
+### Github-repository-Heroku-deployed-link
+
+- Github repository:
+  [Github repository](https://github.com/JessicaPerez1/Note-Taker.git)
+- Heroku Deployed link:
+  [Heroku deployed app link](https://notetakerrrr.herokuapp.com/)
+
+### App-screenshots
+
+![Note Taker screenshot]()
+
+### Questions
+
+For any questions about this app or if you'd like to contribute, please refer to my Github page or contact me by email!
+
+- [JessicaPerez1 Github link:](https://github.com/JessicaPerez1)
+- 1jessicaperez@gmail.com
--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-// Dependencies========================
+// Dependencies=============
 var express = require("express");
 var path = require("path");
 const fs = require("fs");
-// Sets up the Express App========================
+// Sets up the Express App
 var app = express();
 //set up dynamic port for HEROKU
 var PORT = process.env.PORT || 3000;
 //piece of middleware built into Express, finds and returns the static file requested.
-// const { uuid } = require("uuidv4");
-//to use promisify
-// const util = require("util");
 
-// Sets up the Express app to handle data parsing========================
+// Sets up the Express app to handle data parsing
 //pass the name of the directory we want Express to serve files from
 app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
@@ -34,8 +31,7 @@ app.post("/api/notes", function (req, res) {
 
     database = JSON.parse(database);
     var newNote = req.body;
-
-    // Proposition: We want each new id to be one greater than the last elements id
+    // We want each new id to be one greater than the last elements id
     // newNote.id = database.length + 1;
     //If there are no notes, you end up breaking because you can't make a new note id.
     if (database.length === 0) {
@@ -44,19 +40,15 @@ app.post("/api/notes", function (req, res) {
       const lastElementId = database[database.length - 1].id;
       newNote.id = lastElementId + 1;
     }
-
     database.push(newNote);
-
     database = JSON.stringify(database);
-
     fs.writeFile("db/db.json", database, function (err) {
       if (err) throw err;
       res.sendStatus(200);
     });
   });
 });
-
-//DELETE ===============NOT DONE
+//DELETE
 app.delete("/api/notes/:id", function (req, res) {
   const id = parseInt(req.params.id);
   fs.readFile("db/db.json", "utf8", function (err, database) {
@@ -75,10 +67,8 @@ app.delete("/api/notes/:id", function (req, res) {
     });
   });
 });
-
 //HTML ROUTES
 //GET /api/notes - should read the db json file and return all saved notes as JSON
-//get notes page
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
@@ -86,11 +76,7 @@ app.get("/notes", function (req, res) {
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-// Starts the server to begin listening========================
+// Starts the server to begin listening
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
-
-//should receive a new note
-//function to save a note from db
-//get the note data from the inputs and save it to the db and update
